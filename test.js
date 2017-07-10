48c0a88f652a5c2488d45d7852facc686c3e2980
Le Nguyen <nguyenquocdaile@gmail.com>
2017-07-10 21:49:08 +0700

    test server

---
+++ test.js
@@ -0,0 +1,17 @@
+var net = require('net')
+var sockets = []
+net.createServer(function (socket) {
+    sockets.push(socket)
+    socket.write('Talk to me.\n')
+
+    socket.on('data', function (data) {
+        for ( var i = 0; i < sockets.length; ++i ) {
+            sockets[i].write(data)
+        }
+    })
+
+    socket.on('end', function () {
+        var index = sockets.indexOf(socket)
+        sockets = sockets.slice(0, index).concat(sockets.slice(index + 1))
+    })
+}).listen(8000)
